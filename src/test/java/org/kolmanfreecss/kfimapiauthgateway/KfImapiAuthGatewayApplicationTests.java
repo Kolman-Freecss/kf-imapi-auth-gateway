3a72a75e89adc76aaf50a700a83610b8d1f01530
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-10-26 02:52:21 +0200

    First commit

---
+++ src/test/java/org/kolmanfreecss/kfimapiauthgateway/KfImapiAuthGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kolmanfreecss.kfimapiauthgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfImapiAuthGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
